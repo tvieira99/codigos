a84b67a7b304b99ee66ab207cb7eab174d97d022
thiago.vieira <thiago.vieira@ufba.br>
2022-04-28 07:00:43 -0300

    pipe.c

--- UFBA/MATA58_SO/pipe.c
+++ UFBA/MATA58_SO/pipe.c
@@ -1,8 +1,7 @@
-/**
+/*
  * FORMULA DE BÁSKARA UTILIZANDO PIPES!!!!
  * Thiago Vieira Souza Andrade
  */
-#include <sys/sem.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -11,85 +10,61 @@
 #include <sys/types.h>
 
 int main (int argc, char *argv[]){
-	int fd_1[2], fd_2[2], fd_3[2];
-	char readbuffer[80];
-	pid_t son1,son2,son3;
-
-	pipe(fd_1);
-	pipe(fd_2);
-	pipe(fd_3);
+	int fd1[2],fd2[2],fd3[3];	
+	pid_t son1;
+	pipe(fd1);
+	pipe(fd2);
+	pipe(fd3);
 	//son1 irá fazer (-b+sqrt(delta))/2a
 	//son2 irá fazer (-b-sqrt(delta))/2a
-	//son3 irá receber os valores e dar -b sqrt(delta) e 2a
-	close(fd_1[0]);
-	float valorA;
-	scanf("%f", &valorA);
-	float valor2A = 2 * valorA;
-	write(fd_1[1], &valor2A, (sizeof(valor2A))); 	
 	
-	close(fd_2[0]);
-	float valorB;
-	scanf("%f", &valorB);
-	float valorBneg = -valorB;
-	write(fd_2[1], &valorBneg, (sizeof(valorBneg))); 
-
-	close(fd_3[0]);
-	float valorC;
-	scanf("%f", &valorC);
-	float delta = sqrt(valorB*valorB -(4*valorA*valorC));
-	write(fd_3[1], &delta, (sizeof(delta))); 
-	
-	if((son1 = fork()) == -1){
+	son1 = fork();	
+	if(son1 == -1){
 		perror("fork");
 		exit(1);
 	}
 
 	if(son1 == 0){
-		float negB, A2, sqrtDelta;
-		close(fd_1[1]);
-		read(fd_1[0], readbuffer, sizeof(readbuffer));	
-		A2 = atof(readbuffer);
-
-		close(fd_2[1]);
-		read(fd_2[0], readbuffer, sizeof(readbuffer));	
-		negB = atof(readbuffer);
+		float num1, num2, num3;
+		close(fd1[1]);
+		read(fd1[0], &num1, (sizeof(float)));
+		close(fd1[0]);
 		
-		close(fd_3[1]);
-		read(fd_3[0], readbuffer, sizeof(readbuffer));	
-		sqrtDelta = atof(readbuffer);
-
-		float valorTotal = (negB+sqrtDelta)/A2;
-		printf("%f\n", valorTotal);	
-	}
-	
-	if((son2 = fork()) == -1){
-		perror("fork");
-		exit(1);
-	}
-
-	if(son2 == 0){
-		float negB, A2, sqrtDelta;
-		close(fd_1[1]);
-		read(fd_1[0], readbuffer, sizeof(readbuffer));	
-		A2 = atof(readbuffer);
-
-		close(fd_2[1]);
-		read(fd_2[0], readbuffer, sizeof(readbuffer));	
-		negB = atof(readbuffer);
+		close(fd2[1]);
+		read(fd2[0], &num2, (sizeof(float)));
+		close(fd2[0]);
 		
-		close(fd_3[1]);
-		read(fd_3[0], readbuffer, sizeof(readbuffer));	
-		sqrtDelta = atof(readbuffer);
+		close(fd3[1]);
+		read(fd3[0], &num3, (sizeof(float)));
+		close(fd3[0]);
 
-		float valorTotal = (negB-sqrtDelta)/A2;
-		printf("%f\n", valorTotal);	
-	}
-	
-	if((son3 = fork()) == -1){
-		perror("fork");
-		exit(1);
-	}
+		float x1 = (num2 - num3)/num1;
+		printf("%f\n",x1);
+		exit(0);
+	} else {
+		float num1, num2, num3;
+		scanf("%f %f %f", &num1, &num2, &num3);
+		
+		float a2 = num1 * 2;
+		float negB = -num2;
+		float deltPart1 = num2*num2;
+		float deltPart2 = 4*num1*num3;
+		float de = deltPart1 - deltPart2;
+		float delta = sqrtf(de);
+		
+		close(fd1[0]);
+		write(fd1[1], &a2, (sizeof(float)));
+		close(fd1[1]);
 
-	if(son3 == 0){
+		close(fd2[0]);
+		write(fd2[1], &negB, (sizeof(float)));
+		close(fd2[1]);
+
+		close(fd3[0]);
+		write(fd3[1], &delta, (sizeof(float)));
+		close(fd3[1]);
+		
+		float x2 = (negB + delta)/a2;
+		printf("%f\n",x2);
 	}
 }
---
+++ UFBA/MATA58_SO/teste.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]){
+	float num1, num2, num3;
+	scanf("%f %f %f", &num1, &num2, &num3);
+	printf("%f %f %f", num1, num2, num3);
+}
